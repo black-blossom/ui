7297c3f7c7dbce3630809e08da5cea4033607e76
alejandro waumann <awaumann@gmail.com>
2022-11-22 20:20:05 -0600

    Add address and wallet connection status to TopBar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -7,6 +7,7 @@ import {
   Button,
   Stack,
   Toolbar,
+  Tooltip,
   Typography,
 } from '@mui/material';
 
@@ -22,13 +23,18 @@ function TopBar() {
     <>
       <AppBar>
         <Toolbar>
-          <Avatar src={logo} sx={{ bgcolor: 'white', width: 36, height: 36, marginRight: 2 }} />
+          <Avatar src={logo} sx={{ bgcolor: 'white', width: 40, height: 40, marginRight: 2 }} />
           <Typography variant="h6" sx={{ flexGrow: 1 }}>Black Blossom</Typography>
           { auth ? (
-            <Stack direction="row" alignItems="center" spacing={2}>
-              <Badge color="success" badgeContent=" " variant="dot"></Badge>
-              <Typography>Godyl</Typography>
-              <Avatar src={avatar} sx={{ width: 36, height: 36 }} />
+            <Stack direction="row" alignItems="center" spacing={3}>
+              <Tooltip title="Connected to Polygon Mainnet" placement="left">
+                <Badge color="success" badgeContent=" " variant="dot"></Badge>
+              </Tooltip>
+              <Stack direction="column" alignItems="center">
+                <Typography>Godyl</Typography>
+                <Typography variant="caption">0x106...213C</Typography>
+              </Stack>
+              <Avatar src={avatar} sx={{ width: 40, height: 40 }} />
             </Stack>
           ) : (
             <Button variant="outlined" size="small" onClick={ () => setOpenDialog(true) }>Sign in with Wallet</Button>
