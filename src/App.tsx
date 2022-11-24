caf151c525996ad4a6ee4e8d3ffd17b99feac833
alejandro waumann <awaumann@gmail.com>
2022-11-23 22:01:08 -0600

    Add routing using wouter
    * Add routes for all icon toggle buttons in the SideBar
    * Toggle the button for the route that is selected

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Redirect, Route, Switch } from 'wouter';
+
 import {
   Box,
   CssBaseline,
@@ -16,7 +18,47 @@ function App() {
       <TopBar />
       <SideBar />
       <Box sx={{ paddingTop: 4, paddingLeft: 14, paddingRight: 4, paddingBottom: 4 }}>
-        hello world
+        <Switch>
+          <Route path="/">
+            <Redirect to="/feed" />
+          </Route>
+
+          <Route path="/feed">
+            feed
+          </Route>
+
+          <Route path="/notifications">
+            notifications
+          </Route>
+
+          <Route path="/trade">
+            trade
+          </Route>
+
+          <Route path="/arena">
+            arena
+          </Route>
+
+          <Route path="/leaderboard">
+            leaderboard
+          </Route>
+
+          <Route path="/store">
+            store
+          </Route>
+
+          <Route path="/settings">
+            settings
+          </Route>
+
+          <Route path="/docs">
+            docs
+          </Route>
+
+          <Route>
+            404 error
+          </Route>
+        </Switch>
       </Box>
     </ThemeProvider>
   );
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,5 @@
+import { Link, useLocation } from 'wouter';
+
 import {
   Avatar,
   Box,
@@ -22,6 +24,7 @@ import {
 import logo from './../assets/logo.png';
 
 const SideBar = () => {
+  const [location, setLocation] = useLocation();
 
   return (
     <Drawer variant="permanent" elevation={0}>
@@ -29,58 +32,74 @@ const SideBar = () => {
         <Stack direction="column" alignItems="center" justifyContent="flex-start" spacing={2}>
           <Avatar src={logo} sx={{ bgcolor: 'white', width: 40, height: 40, marginBottom: 3 }} />
 
-          <Tooltip title="Feed" placement="right" enterDelay={500} enterNextDelay={500} arrow>
-            <ToggleButton value="feed" sx={{ border: 0 }} selected>
-              <TimelineOutlined />
-            </ToggleButton>
-          </Tooltip>
+          <Link to="/feed">
+            <Tooltip title="Feed" placement="right" enterDelay={500} enterNextDelay={500} arrow>
+              <ToggleButton value="feed" sx={{ border: 0 }} selected={ location.split('/')[1] === 'feed' }>
+                <TimelineOutlined />
+              </ToggleButton>
+            </Tooltip>
+          </Link>
 
-          <Tooltip title="Notifications" placement="right" enterDelay={500} enterNextDelay={500} arrow>
-            <ToggleButton value="notifications" sx={{ border: 0 }} >
-              <NotificationsOutlined />
-            </ToggleButton>
-          </Tooltip>
+          <Link to="/notifications">
+            <Tooltip title="Notifications" placement="right" enterDelay={500} enterNextDelay={500} arrow>
+              <ToggleButton value="notifications" sx={{ border: 0 }} selected={ location.split('/')[1] === 'notifications' }>
+                <NotificationsOutlined />
+              </ToggleButton>
+            </Tooltip>
+          </Link>
 
-          <Tooltip title="Trade" placement="right" enterDelay={500} enterNextDelay={500} arrow>
-            <ToggleButton value="trade" sx={{ border: 0 }} >
-              <CandlestickChartOutlined />
-            </ToggleButton>
-          </Tooltip>
+          <Link to="/trade">
+            <Tooltip title="Trade" placement="right" enterDelay={500} enterNextDelay={500} arrow>
+              <ToggleButton value="trade" sx={{ border: 0 }} selected={ location.split('/')[1] === 'trade' }>
+                <CandlestickChartOutlined />
+              </ToggleButton>
+            </Tooltip>
+          </Link>
 
-          <Tooltip title="Arena" placement="right" enterDelay={500} enterNextDelay={500} arrow>
-            <ToggleButton value="arena" sx={{ border: 0 }} >
-              <StadiumOutlined />
-            </ToggleButton>
-          </Tooltip>
+          <Link to="/arena">
+            <Tooltip title="Arena" placement="right" enterDelay={500} enterNextDelay={500} arrow>
+              <ToggleButton value="arena" sx={{ border: 0 }} selected={ location.split('/')[1] === 'arena' }>
+                <StadiumOutlined />
+              </ToggleButton>
+            </Tooltip>
+          </Link>
 
-          <Tooltip title="Leaderboard" placement="right" enterDelay={500} enterNextDelay={500} arrow>
-            <ToggleButton value="leaderboard" sx={{ border: 0 }} >
-              <LeaderboardOutlined />
-            </ToggleButton>
-          </Tooltip>
+          <Link to="/leaderboard">
+            <Tooltip title="Leaderboard" placement="right" enterDelay={500} enterNextDelay={500} arrow>
+              <ToggleButton value="leaderboard" sx={{ border: 0 }} selected={ location.split('/')[1] === 'leaderboard' }>
+                <LeaderboardOutlined />
+              </ToggleButton>
+            </Tooltip>
+          </Link>
 
-          <Tooltip title="Store" placement="right" enterDelay={500} enterNextDelay={500} arrow>
-            <ToggleButton value="store" sx={{ border: 0 }} >
-              <StorefrontOutlined />
-            </ToggleButton>
-          </Tooltip>
+          <Link to="/store">
+            <Tooltip title="Store" placement="right" enterDelay={500} enterNextDelay={500} arrow>
+              <ToggleButton value="store" sx={{ border: 0 }} selected={ location.split('/')[1] === 'store' }>
+                <StorefrontOutlined />
+              </ToggleButton>
+            </Tooltip>
+          </Link>
 
           <Stack direction="column" alignItems="center" sx={{ position: 'absolute', bottom: 16 }}>
 
-            <Tooltip title="Settings" placement="right" enterDelay={500} enterNextDelay={500} arrow>
-              <ToggleButton value="settings" sx={{ border: 0 }} >
-                <SettingsOutlined />
-              </ToggleButton>
-            </Tooltip>
+            <Link to="/settings">
+              <Tooltip title="Settings" placement="right" enterDelay={500} enterNextDelay={500} arrow>
+                <ToggleButton value="settings" sx={{ border: 0 }} selected={ location.split('/')[1] === 'settings' }>
+                  <SettingsOutlined />
+                </ToggleButton>
+              </Tooltip>
+            </Link>
 
-            <Tooltip title="Docs" placement="right" enterDelay={500} enterNextDelay={500} arrow>
-              <ToggleButton value="docs" sx={{ border: 0 }} >
-                <ArticleOutlined />
-              </ToggleButton>
-            </Tooltip>
+            <Link to="/docs">
+              <Tooltip title="Docs" placement="right" enterDelay={500} enterNextDelay={500} arrow>
+                <ToggleButton value="docs" sx={{ border: 0 }} selected={ location.split('/')[1] === 'docs' }>
+                  <ArticleOutlined />
+                </ToggleButton>
+              </Tooltip>
+            </Link>
 
             <Tooltip title="Logout" placement="right" enterDelay={500} enterNextDelay={500} arrow>
-              <ToggleButton value="logout" sx={{ border: 0 }} >
+              <ToggleButton value="logout" sx={{ border: 0 }}>
                 <LogoutOutlined />
               </ToggleButton>
             </Tooltip>
