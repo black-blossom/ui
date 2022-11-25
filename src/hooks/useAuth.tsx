2294de9be00aecb906df1f4f0fcdbe38bbd3dd39
alejandro waumann <awaumann@gmail.com>
2022-11-25 17:11:27 -0600

    Add useAuth hook

---
+++ src/hooks/useAuth.tsx
@@ -0,0 +1,29 @@
+import create from 'zustand';
+
+interface IUserData {
+  auth: boolean;
+  address: string;
+  username: string;
+};
+
+interface IAuthStore {
+  data: IUserData;
+  authenticate: () => void;
+  deauhenticate: () => void;
+};
+
+const useAuthStore = create<IAuthStore>((set) => ({
+  data: {
+    auth: true,
+    address: '',
+    username: 'Godyl'
+  },
+  authenticate: () => {
+    set({data: { auth: true, address: '0x10628634d540a3b9daa77176b1d68ac875c3213c', username: 'Godyl' }});
+  },
+  deauhenticate: () => {
+    set({data: { auth: false, address: '', username: '' }});
+  },
+}));
+
+export default useAuthStore;
