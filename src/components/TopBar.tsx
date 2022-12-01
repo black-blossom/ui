e930f19bd0cbf36846c7124efe98152ecdab722b
alejandro waumann <awaumann@gmail.com>
2022-12-01 02:03:03 -0600

    Remove remaining login button logic from TopBar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,20 +1,15 @@
-import { useState } from 'react';
 import {
   AppBar,
   Box,
-  Button,
   Toolbar,
-  Tooltip,
 } from '@mui/material';
 
 import AccountBar from './AccountBar';
 import LoginButton from './LoginButton';
 import NetworkSelector from './NetworkSelector';
 import useAuthStore from './../hooks/useAuth';
-import useNetworkStore from './../hooks/useNetwork';
 
 const TopBar = () => {
-  const web3Available = useNetworkStore(state => state.web3Available);
   const user = useAuthStore(state => state.data);
 
   // TODO: add title of pages here
@@ -24,17 +19,7 @@ const TopBar = () => {
         <Box sx={{ marginLeft: 11 }} />
         <Box sx={{ flexGrow: 1 }} />
         <NetworkSelector />
-        { user.auth ? (
-          <AccountBar />
-        ) : web3Available ? (
-          <LoginButton />
-        ) : (
-          <Tooltip title="No wallet available" placement="bottom">
-            <span>
-              <Button variant="outlined" size="small" disabled>Login</Button>
-            </span>
-          </Tooltip>
-        )}
+        { user.auth ? <AccountBar /> : <LoginButton /> }
       </Toolbar>
     </AppBar>
   );
