77564fd2d80ab33c0b4e2ea3357e940dbbb3fc51
alejandro waumann <awaumann@gmail.com>
2022-12-03 02:08:16 -0600

    Add the network connector
    * Export network and its hooks
    * Add to Web3ReactProvider connectors prop

---
+++ src/connectors/network.ts
@@ -0,0 +1,15 @@
+import { initializeConnector } from '@web3-react/core';
+import { Network } from '@web3-react/network';
+import { URLS } from '../utils/chains';
+
+export const [network, hooks] = initializeConnector<Network>(
+  (actions) => new Network({ actions, urlMap: URLS })
+);
+
+export const {
+  useChainId,
+  useAccounts,
+  useIsActivating,
+  useIsActive,
+  useProvider
+} = hooks;
--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { hooks as metaMaskHooks, metamask } from './connectors/metamask';
+import { hooks as networkHooks, network } from './connectors/network';
 import { MetaMask } from '@web3-react/metamask';
+import { Network } from '@web3-react/network';
 import { Web3ReactHooks, Web3ReactProvider } from '@web3-react/core';
 
 import App from './App';
 import './index.css';
 
-const connectors: [MetaMask, Web3ReactHooks][] = [
+const connectors: [MetaMask | Network, Web3ReactHooks][] = [
   [metamask, metaMaskHooks],
+  [network, networkHooks]
 ];
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
