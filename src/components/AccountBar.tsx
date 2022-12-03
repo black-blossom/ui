b298eda61824a486e9af17b0f5595749a6205584
alejandro waumann <awaumann@gmail.com>
2022-12-03 01:59:24 -0600

    Show the correct account address in AccountBar

--- src/components/AccountBar.tsx
+++ src/components/AccountBar.tsx
@@ -8,16 +8,20 @@ import {
 import avatar from '../assets/avatar.png';
 import NetworkStatusBadge from '../components/NetworkStatusBadge';
 import useAuthStore from '../hooks/useAuth';
+import { useAccounts } from '../connectors/metamask';
 
 const AccountBar = () => {
   const user = useAuthStore(state => state.data);
+  const accounts = useAccounts();
+
+  const address = accounts ? `${accounts[0].slice(0, 5)}...${accounts[0].slice(38, 42)}` : '';
 
   // TODO: separate out the wallet info from the user avatar
   return (
-    <Stack direction="row" alignItems="center" spacing={3}>
+    <Stack direction="row" alignItems="center" spacing={2}>
       <Stack direction="column" alignItems="center">
         <Typography variant="body2">{user.username}</Typography>
-        <Typography variant="caption">{`${user.address.slice(0, 5)}...${user.address.slice(38, 42)}`}</Typography>
+        <Typography variant="caption">{address}</Typography>
       </Stack>
       <ButtonBase>
         <NetworkStatusBadge>
--- src/components/NetworkStatusBadge.tsx
+++ src/components/NetworkStatusBadge.tsx
@@ -1,18 +1,19 @@
 import { Badge } from '@mui/material';
 
-import { useChainId } from '../connectors/metamask';
+import { useChainId, useIsActive } from '../connectors/metamask';
 import useNetworkStore from '../hooks/useNetwork';
 
 const NetworkStatusBadge = ({ children }: any) => {
   const appChainId = useNetworkStore(state => state.chainId);
   const walletChainId = useChainId();
+  const isActive = useIsActive();
 
   return (
     <Badge
       variant="dot"
       overlap="circular"
       anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-      color={appChainId === walletChainId ? 'success' : 'error'}
+      color={appChainId === walletChainId && isActive ? 'success' : 'error'}
     >
       {children}
     </Badge>
