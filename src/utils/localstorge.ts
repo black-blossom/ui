5487bb84c138b847fab0a1b7a7dfbff28a41e940
alejandro waumann <awaumann@gmail.com>
2022-12-03 04:42:05 -0600

    Add utility for using localstorage

---
+++ src/utils/localstorge.ts
@@ -0,0 +1,10 @@
+export const getLocalStorage = (key: string, defaultValue: any) => {
+  const value = localStorage.getItem(key);
+  if(value === null || value === undefined) return defaultValue;
+  return JSON.parse(value);
+};
+
+export const setLocalStorage = (key: string, value: any) => {
+  if(value === null || value === undefined) return;
+  localStorage.setItem(key, JSON.stringify(value));
+};
