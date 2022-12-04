4f629b5f1bf150e27bc46b5227a7551285187892
alejandro waumann <awaumann@gmail.com>
2022-12-04 02:46:19 -0600

    Remove TODO comments

--- src/components/NetworkManager.tsx
+++ src/components/NetworkManager.tsx
@@ -3,8 +3,6 @@ import { useEffect } from 'react';
 import useNetworkStore from '../hooks/useNetwork';
 import useAuthStore from '../hooks/useAuth';
 
-// TODO: add toast message to switch network when there's a mismatch
-// TODOO: fix issue where isActive is false but wallet is actually connected to RPC
 const NetworkManager = () => {
   const targetChainId = useNetworkStore(state => state.targetChainId);
   const connectNetwork = useNetworkStore(state => state.connectNetwork);
